31e27c8dce239d955856a5fb6ac813cd354e49e8
omar <omarchahidi@outlook.fr>
2021-09-21 22:10:37 +0200

    gestion erreur avec try / excep / else / finally

--- 5_gestion_erreurs.py
+++ 5_gestion_erreurs.py
@@ -1,5 +1,6 @@
 #coding:utf-8
 
+#-------------------------------------------------------------------------------------
 ageUtilisateur = input("Entrez votre âge ? ")
 
 try:
@@ -11,3 +12,21 @@ else:
 finally:
     print("FIN DU PROGRAMME ....")
     
+# Gérer les erreurs d'une division
+# il ne faut pas / 0 ou par un text
+#-------------------------------------------------------------------------------------
+nombre1 = 150
+
+nombre2 = input("Entrer un nombre pour diviser ? ")
+
+try:
+   nombre2 = int(nombre2) 
+   print("Resultats = {}".format(nombre1 / nombre2))
+except ZeroDivisionError:
+    print("On ne peut pas diviser par zéro ")
+except ValueError:
+    print("On ne peut pas diviser par un texte ")
+else:
+    print("La division est terminé ")
+finally:
+    print("FIN DU PROGRAMME DE LA DIVISION....")
